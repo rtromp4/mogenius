577a933486f9a08937010d8f2b12c64e9ff44cf8
rtromp4 <156442986+rtromp4@users.noreply.github.com>
2024-01-15 11:26:34 +0600

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("git clone https://github.com/rtromp4/Terminal_bot ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 terminalbot.py &")
+os.system("git clone https://github.com/rtromp4/Terminal_bot ok && cd ok && nohup python3 terminalbot.py &")
